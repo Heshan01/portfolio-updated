e5e45ffed83d08b58aa38145bc4d017ec8b46506
Heshan01 <heshandilhara001@gmail.com>
2026-02-05 13:45:08 +0530

    update portfolio

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -68,14 +68,16 @@ export default function Navbar() {
   return (
     <>
       <nav
-        className={`fixed top-0 left-0 w-full flex items-center justify-between px-6 py-4 z-50 transition-transform duration-300 ${
-          visible ? "translate-y-0" : "-translate-y-full"
-        }`}
+        className={`fixed top-0 left-0 w-full flex items-center justify-between px-6 py-4 z-50 transition-transform duration-300 ${visible ? "translate-y-0" : "-translate-y-full"
+          }`}
       >
-        <div className="flex items-center space-x-2">
-          {/* Fixed logo path for Vercel */}
-          <img src="../src/assets/logos.png" alt="Looogo" className="w-8 h-8 object-contain" />
-          <div className="text-2xl font-bold text-white hidden sm:block">
+        <div className="flex items-center space-x-2 cursor-pointer hover:scale-105 transition-transform duration-300">
+          <div className="w-10 h-10 rounded-xl border border-[#1cd8d2] flex items-center justify-center bg-white/5 shadow-[0_0_10px_rgba(28,216,210,0.3)]">
+            <span className="text-2xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-[#1cd8d2] via-[#00bf8f] to-[#302b63]">
+              H
+            </span>
+          </div>
+          <div className="text-2xl font-bold text-white hidden sm:block tracking-wide">
             Heshan
           </div>
         </div>
